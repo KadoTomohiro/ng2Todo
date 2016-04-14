7d1bb0ebc04144f3a87fd3f286e39bd34c5ca3f6
KadoTomohiro <kado.tomohiro@jp.fujitsu.com>
2016-04-14 16:44:50 +0900

    イベント発行の実装

--- components/home/home.ts
+++ components/home/home.ts
@@ -1,13 +1,33 @@
 import {Component} from 'angular2/core'
+import {AddTask} from '../addTask/addTask'
 
 @Component({
   selector: 'my-app',
   template: `
-    <h1>My First {{name}} {{1+1}} App</h1>
-    `
+    <h1>My First {{name}} {{1+1}} Todo App</h1>
+    <ul>
+      <li *ngFor="#task of tasks">
+        {{task}}
+      </li>
+    </ul>
+    <add-task (addTask)="add($event)" (emptyTask)="emptyAdd()"></add-task>
+    {{message}}
+    `,
+  directives: [AddTask]
 })
 
 export class AppComponent {
   public name = 'Angular';
+  public tasks = [];
+  public message: string = '';
+
+  add(newTask) {
+    this.tasks.push(newTask);
+  }
+
+  emptyAdd() {
+    this.message = 'タスクを入力してください';
+    setTimeout(() => this.message = '', 5000);
+  }
 }
 　
